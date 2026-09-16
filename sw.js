bfb7e77b786f70833b3c818d208bc1b4f2f8f262
cocossurvey <cocos.survey@gmail.com>
2026-09-16 17:24:17 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "cocos-survey-v11";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  if (req.mode === "navigate") {
+    e.respondWith(caches.match("./").then(r => r || fetch(req).then(res => { const cp = res.clone(); caches.open(CACHE).then(c => c.put("./", cp)); return res; })));
+    return;
+  }
+  e.respondWith(caches.match(req).then(r => r || fetch(req).then(res => { const cp = res.clone(); caches.open(CACHE).then(c => c.put(req, cp)); return res; }).catch(() => r)));
+});
